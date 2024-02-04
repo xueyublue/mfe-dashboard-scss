2bc71fd3ef76dc72830c36b27508cfd3bec1c2d3
Darren <xueyublue@gmail.com>
2024-02-04 12:29:39 +0800

    added router, added users and products pages

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 npm create vite .np
 npm install
 npm install sass
+npm install react-router-dom
 
 npm run dev
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Products from "./pages/products/Products";
+import Users from "./pages/users/Users";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "users",
+    element: <Users />,
+  },
+  {
+    path: "products",
+    element: <Products />,
+  },
+]);
 
 function App() {
   return (
     <>
-      <Home />
+      <RouterProvider router={router} />
     </>
   );
 }
