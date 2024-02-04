2bdf7419bc2e5f611b1caa27646df7b560a1773d
Darren <xueyublue@gmail.com>
2024-02-04 19:12:06 +0800

    1) added pictures, 2) completed top navbar

--- src/components/navbar/Navabr.jsx
+++ src/components/navbar/Navabr.jsx
@@ -1,7 +1,31 @@
 import "./navbar.scss";
 
 const Navbar = () => {
-  return <div className="navbar">Navnar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="" />
+        <span>React Dashboard with SASS</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="" className="icon" />
+          <span>2</span>
+        </div>
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+            alt=""
+          />
+          <span>Dev User</span>
+        </div>
+        <img src="/settings.svg" alt="" className="icon" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
