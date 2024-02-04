2c8c7a53cc67df5026b787bc1468c090e53f404b
Darren <xueyublue@gmail.com>
2024-02-04 13:14:55 +0800

    added layout, routings

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,52 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Products from "./pages/products/Products";
 import Users from "./pages/users/Users";
+import Navbar from "./components/navbar/Navabr";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import Login from "./pages/login/Login";
+import "./styles/global.scss";
+
+const Layout = () => {
+  return (
+    <div className="main">
+      <Navbar />
+      <div className="container">
+        <div className="menuContainer">
+          <Menu />
+        </div>
+        <div className="contentContainer">
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/users",
+        element: <Users />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      },
+    ],
   },
   {
-    path: "users",
-    element: <Users />,
-  },
-  {
-    path: "products",
-    element: <Products />,
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
---
+++ src/components/navbar/Navabr.jsx
@@ -0,0 +1,7 @@
+import "./navbar.scss";
+
+const Navbar = () => {
+  return <div className="navbar">Navnar</div>;
+};
+
+export default Navbar;
