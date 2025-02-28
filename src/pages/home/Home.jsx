8d74183c6d727abd76237e247b1e24c10197cc9f
Darren <xueyublue@gmail.com>
2025-02-28 20:01:04 +0800

    temp update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,19 @@
 import "./home.scss";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <div className="box box1">box 1</div>
+      <div className="box box2">box 2</div>
+      <div className="box box3">box 3</div>
+      <div className="box box4">box 4</div>
+      <div className="box box5">box 5</div>
+      <div className="box box6">box 6</div>
+      <div className="box box7">box 7</div>
+      <div className="box box8">box 8</div>
+      <div className="box box9">box 9</div>
+    </div>
+  );
 };
 
 export default Home;
